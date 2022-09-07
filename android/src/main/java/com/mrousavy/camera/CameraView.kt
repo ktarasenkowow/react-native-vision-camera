0d9d50579b670f6bdb7cc6cdd1456b15267d28ff
Tarasenko Kirill <tarasenko-kirill@ro.ru>
2022-09-07 14:16:36 +0300

    Android: Dynamic prop exposure added

--- android/src/main/java/com/mrousavy/camera/CameraView.kt
+++ android/src/main/java/com/mrousavy/camera/CameraView.kt
@@ -257,7 +257,6 @@ class CameraView(context: Context, private val frameProcessorThread: ExecutorSer
   }
 
   override fun onConfigurationChanged(newConfig: Configuration?) {
-
     super.onConfigurationChanged(newConfig)
     updateOrientation()
   }
@@ -281,7 +280,6 @@ class CameraView(context: Context, private val frameProcessorThread: ExecutorSer
    * Updates the custom Lifecycle to match the host activity's lifecycle, and if it's active we narrow it down to the [isActive] and [isAttachedToWindow] fields.
    */
   private fun updateLifecycleState() {
-
     val lifecycleBefore = lifecycleRegistry.currentState
     if (hostLifecycleState == Lifecycle.State.RESUMED) {
       // Host Lifecycle (Activity) is currently active (RESUMED), so we narrow it down to the view's lifecycle
@@ -309,7 +307,7 @@ class CameraView(context: Context, private val frameProcessorThread: ExecutorSer
   override fun onDetachedFromWindow() {
     super.onDetachedFromWindow()
     updateLifecycleState()
-    lifecycleRegistry.currentState = Lifecycle.State.DESTROYED
+    lifecycleRegistry.currentState = Lifecycle.State.DESTROYED 
   }
 
   fun updateExposure() {
@@ -324,7 +322,6 @@ class CameraView(context: Context, private val frameProcessorThread: ExecutorSer
         camera?.cameraControl?.setExposureCompensationIndex((exposure!! * EXPOSURE_VALUE_RATIO).toInt())
     };
   }
-
   /**
    * Invalidate all React Props and reconfigure the device
    */
@@ -334,7 +331,6 @@ class CameraView(context: Context, private val frameProcessorThread: ExecutorSer
     //  I need to use CoroutineScope.launch because of the suspend fun [configureSession]
     coroutineScope.launch {
       try {
-
         val shouldReconfigureSession = changedProps.containsAny(propsThatRequireSessionReconfiguration)
         val shouldReconfigureZoom = shouldReconfigureSession || changedProps.contains("zoom")
         val shouldReconfigureTorch = shouldReconfigureSession || changedProps.contains("torch")
@@ -358,7 +354,7 @@ class CameraView(context: Context, private val frameProcessorThread: ExecutorSer
           updateOrientation()
         }
         if (shouldUpdateExposure) {
-          updateExposure();
+          updateExposure()
         }
       } catch (e: Throwable) {
         Log.e(TAG, "update() threw: ${e.message}")
@@ -495,13 +491,23 @@ class CameraView(context: Context, private val frameProcessorThread: ExecutorSer
       if (photo == true) {
         if (fallbackToSnapshot) {
           Log.i(TAG, "Tried to add photo use-case (`photo={true}`) but the Camera device only supports " +
-            "a single use-case at a time. Falling back to Snapshot capture.")
+                  "a single use-case at a time. Falling back to Snapshot capture.")
         } else {
           Log.i(TAG, "Adding ImageCapture use-case...")
           imageCapture = imageCaptureBuilder.build()
           useCases.add(imageCapture!!)
         }
       }
+      val exposureState: ExposureState? = camera?.cameraInfo?.exposureState;
+      val range = exposureState?.exposureCompensationRange;
+      Log.d("got exposure ", exposure.toString());
+      val MAX_PROP_RANGE = 100;
+      if (range != null && exposure != null) {
+        val LOW_RANGE = Math.min(Math.abs(range!!.lower), Math.abs(range!!.upper));
+        val EXPOSURE_VALUE_RATIO = LOW_RANGE.toDouble() / MAX_PROP_RANGE
+        Log.i("finish exposure ", (exposure!! * EXPOSURE_VALUE_RATIO).toInt().toString());
+        camera?.cameraControl?.setExposureCompensationIndex((exposure!! * EXPOSURE_VALUE_RATIO).toInt())
+      };
 
       if (enableFrameProcessor) {
         Log.i(TAG, "Adding ImageAnalysis use-case...")
@@ -534,8 +540,8 @@ class CameraView(context: Context, private val frameProcessorThread: ExecutorSer
 
       minZoom = camera!!.cameraInfo.zoomState.value?.minZoomRatio ?: 1f
       maxZoom = camera!!.cameraInfo.zoomState.value?.maxZoomRatio ?: 1f
-
-      updateExposure();
+      
+      updateExposure()
 
       val duration = System.currentTimeMillis() - startTime
       Log.i(TAG_PERF, "Session configured in $duration ms! Camera: ${camera!!}")
